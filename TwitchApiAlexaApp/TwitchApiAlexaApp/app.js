a244b43fab9328ffc08edb820972c488d83994f6
schlagbm <schlagbm@mail.uc.edu>
2017-06-01 20:50:41 -0400

    Code

--- TwitchApiAlexaApp/TwitchApiAlexaApp/app.js
+++ TwitchApiAlexaApp/TwitchApiAlexaApp/app.js
@@ -1,47 +1,65 @@
-var request = require('request');
+var AlexaLambdaHandler = require('./lib/alexa');
 
-var clientId = "dupvdxyso3tod5js96yf7tq92u3075";
-var streamers = ["A_Seagull", "Lirik", "summit1g", "sodapoppin", "TSM_Bjergsen", "NALCS1", "C9Sneaky", "MOONMOON_OW", "mendokusaii", "Meteos"];
-var totalStreamers = streamers.length;
+module.exports.handler = AlexaLambdaHandler.LambdaHandler;
+module.exports.CreateStateHandler = AlexaLambdaHandler.CreateStateHandler;
+module.exports.StateString = AlexaLambdaHandler.StateString;
 
-var activeStreamers = [];
-var inactiveStreamers = [];
-main();
+var Alexa = require("alexa-sdk");
+var request = require("request");
+var rp = require("request-promise");
+//-----------------------------------INITIALIZE SKILL-----------------------------------//
+exports.handler = function (event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+};
 
-function main() {
-    activeStreamers = [];
-    inactiveStreamers = [];
-    for (i = 0; i < totalStreamers; i++) {
-        var url = 'https://api.twitch.tv/kraken/streams/' + streamers[i] + '?client_id=' + clientId;
-        getStatus(url, streamers[i], i);
-    }
-    DisplayActiveStreamers();
-}
+var handlers = {
+    "LaunchRequest": function () {
+        this.emit(":ask", "Welcome to the twich api!");
+    },
+    "getCurrentStreamers": function () {
+        var clientId = "dupvdxyso3tod5js96yf7tq92u3075";
+        var streamers = ["A_Seagull", "Lirik", "summit1g", "sodapoppin", "TSM_Bjergsen", "NALCS1", "C9Sneaky", "MOONMOON_OW", "mendokusaii", "Meteos"];
+        var totalStreamers = streamers.length;
+        var forLoopTotal = (totalStreamers - 1);
+        var activeStreamers = [];
+        var inactiveStreamers = [];
+        var streamerString = "";
+        let counter = 0;
 
-function getStatus(url, streamer, i) {
-    request(url, function (error, response, body) {
-        var json = JSON.parse(body);
-        if (json.stream == null) {
-            inactiveStreamers.push(streamer);
-        }
-        else {
-            console.log(streamer + " is streaming " + json.stream.game);
-            activeStreamers.push({
-                key: streamer,
-                value: json.stream.game
+
+        for (let i = 0; i < totalStreamers; i++) {
+            var url = 'https://api.twitch.tv/kraken/streams/' + streamers[i] + '?client_id=' + clientId;
+            rp(url, function (error, response, body) {
+                var json = JSON.parse(body);
+                if (json.stream == null) {
+                    inactiveStreamers.push(streamers[i]);
+                    if (counter == forLoopTotal) {
+                        this.emit(":tell", streamerString);
+                    }
+                    counter += 1;
+                }
+                else {
+                    streamerString += json.stream.channel.name + " is streaming " + json.stream.game + ". ";
+                    activeStreamers.push({
+                        key: json.stream.channel.name,
+                        value: json.stream.game
+                    });
+                    if (counter == forLoopTotal) {
+                        this.emit(":tell", streamerString);
+                    }
+                    counter += 1;
+                }
             });
+
         }
-    });
-}
-
-function DisplayActiveStreamers() {
-    for (i = 0; i < activeStreamers.length; i++) {
-        console.log(activeStreamers[i].key + ' on pin ');
     }
-}
+};
 
-function DisplayInactiveStreamers() {
-    for (i = 0; i < inactiveStreamers.length; i++) {
-        console.log(inactiveStreamers[i]);
-    }
-}
+
+//-----------------------------------SETUP HANDLERS-----------------------------------//
+exports.handler = function (event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    //alexa.dynamoDBTableName = "RememberData";
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
